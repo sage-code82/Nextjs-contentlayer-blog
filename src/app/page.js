38c4144b6d739eb63247bec95147b1321aa76411
Stephen <sagelvl40@gmail.com>
2024-01-01 11:40:57 +0000

    set up CSS and Fonts for now

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p024">
+      Hello There!
+    </main>
+  );
 }
